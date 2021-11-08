ee085d26f05744135689a4d83bb64f41978bfbdc
Ubayd-Hosenbocus <19530952@student.curtin.edu.au>
2021-11-08 18:38:17 +0400

    third commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -14296,7 +14296,7 @@ class App {
   constructor() {
     this.name = "Kafe Lakot";
     this.version = "1.0.0";
-    this.apiBase = 'http://localhost:3000';
+    this.apiBase = 'https://uhosenbocus-kafelakot-backend.herokuapp.com/';
     this.rootEl = document.getElementById("root");
     this.version = "1.0.0";
   }
@@ -16275,7 +16275,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64495" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64937" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
